9e77135efadb582e1859bb5396e852b1cfedf478
Sripada Shiva Koushik <126230852+sripada-Shiva-Koushik@users.noreply.github.com>
2024-12-03 15:06:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'GANESHGANNU',
+    password: '',
     database: 'bharat',
 });
 
